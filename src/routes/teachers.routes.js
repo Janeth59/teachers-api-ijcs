60184af8ce006ba74071b9a4ed379779d907c8cd
Janeth59 <d22390101@huauchinango.tecnm.mx>
2025-11-20 11:57:35 -0600

    regresoJSON

--- src/routes/teachers.routes.js
+++ src/routes/teachers.routes.js
@@ -3,10 +3,10 @@ import { Router } from "express";
 import teachersControllers from "../controllers/teachers.controllers.js";
 const router = Router();
 
-router.get("/getAll", teachersControllers.getAll); 
-router.get("/getOne/:teacher_id", teachersControllers.getOne);
-router.post("/insertOne", teachersControllers.insertOne);
-router.post("/updateOne/:teacher_id", teachersControllers.updateOne);
-router.get("/deleteOne/:teacher_id", teachersControllers.deleteOne);
+router.get("/getAll", teachersControllers.getAll); //get
+router.get("/getOne/:teacher_id", teachersControllers.getOne); //get
+router.post("/insertOne", teachersControllers.insertOne); //post
+router.put("/updateOne/:teacher_id", teachersControllers.updateOne); //post
+router.delete("/deleteOne/:teacher_id", teachersControllers.deleteOne); //get
 
 export default router;
